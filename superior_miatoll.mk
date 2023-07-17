8d52db0035b106382ed9f88e430d9671878f1e5d
Swapnil Vicky <swapnil.motoe@gmail.com>
2023-07-17 08:22:48 +0530

    miatoll: SuperiorOS

--- superior_miatoll.mk
+++ superior_miatoll.mk
@@ -13,12 +13,14 @@
 # limitations under the License.
 
 $(call inherit-product, device/xiaomi/miatoll/device.mk)
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Inherit some common superior stuff.
+$(call inherit-product, vendor/superior/config/common.mk)
 
 # Bootanimation Resolution
 TARGET_BOOT_ANIMATION_RES := 1080
 
-PRODUCT_NAME := lineage_miatoll
+PRODUCT_NAME := superior_miatoll
 PRODUCT_DEVICE := miatoll
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := SM6250
@@ -26,3 +28,4 @@ PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 TARGET_SUPPORTS_GOOGLE_RECORDER := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
